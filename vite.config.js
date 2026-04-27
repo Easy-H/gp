9aa3a625e1d8ef5b789f5954c7952df7ec0910d0
EasyH <skysea001010@gmail.com>
2026-04-27 19:19:44 +0900

    github action 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,21 @@ export default defineConfig({
   plugins: [react()],
   base: '/gp/',
   define: {
-    global: 'window',
-    process: {
-      env: { NODE_ENV: 'production' },
-      version: 'v16.0.0',
-      versions: {},
-      platform: 'browser'
-    }
+    'global': 'window',
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': '({ NODE_ENV: "production" })',
+    'process.platform': '"browser"',
+    'process.version': '"v16.0.0"',
+    'process.versions': '{}',
+    'process.browser': 'true',
+    // 전역 변수 'process' 자체를 참조하는 코드를 위한 catch-all 설정
+    'process': '({ env: { NODE_ENV: "production" }, platform: "browser", version: "v16.0.0", versions: {}, browser: true })'
   },
   resolve: {
     alias: {
+      process: 'process/browser',
       crypto: 'crypto-browserify',
-      stream: 'stream-browserify', // Add alias for stream module
-
+      stream: 'stream-browserify',
     },
   },
   optimizeDeps: {
